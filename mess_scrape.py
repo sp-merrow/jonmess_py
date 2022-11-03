78aa6ae491940edca65e76d7d970f22b718775d8
Spencer Merrow <smerrow@disroot.org>
2022-11-03 15:45:57 -0400

    wip

--- mess_scrape.py
+++ mess_scrape.py
@@ -2,11 +2,14 @@ from requests import get
 from bs4 import BeautifulSoup
 from json import loads
 
-songPageList = []
+songUrls = []
 for i in range (1, 9):
     response = get('https://genius.com/api/artists/49350/songs?page={}&sort=popularity'.format(i)).content
     parsedResponse = loads(response)
     for s in parsedResponse['response']['songs']:
-        songPageList.append(s)
+        songUrls.append(s['url'])
 
-print(len(songPageList))
+for url in songUrls:
+    print(url)
+    # response = get(url).text
+    # print(parsedResponse)
