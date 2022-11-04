3607ef7dfe7dd0a3cc6b205c4b56080bb4db24cb
Spencer Merrow <smerrow@disroot.org>
2022-11-04 09:57:28 -0400

    wip

--- mess_scrape.py
+++ mess_scrape.py
@@ -4,6 +4,9 @@ from json import loads
 
 messLyrics = []
 songUrls = []
+
+notLyrics = {';', 'Tillian', ']', 'Kurt Travis', 'Jonny Craig'}
+
 for i in range (1, 9):
     response = get('https://genius.com/api/artists/49350/songs?page={}&sort=popularity'.format(i)).content
     parsedResponse = loads(response)
@@ -14,40 +17,51 @@ for i in range (1, 9):
 for page in songUrls:
     messStuff = []
     workingPage = get(page).content
-    soupLyrics = BeautifulSoup(workingPage, "html.parser").find('div', attrs={"class":"Lyrics__Container-sc-1ynbvzw-6 YYrds"})
+    soupLyrics = BeautifulSoup(workingPage, "html.parser").find_all('div', attrs={"class":"Lyrics__Container-sc-1ynbvzw-6 YYrds"})
 
-    if 'Jon Mess' not in soupLyrics.text:
+    if 'Jon Mess' not in soupLyrics[0].text or 'Jon Mess' not in soupLyrics[1].text:
         continue
 
-    italics = soupLyrics.find_all('i')
+    for lyricSheet in soupLyrics:
+        italics = lyricSheet.find_all('i')
 
-    messy = False
-    for ele in italics:
-        if 'Jon Mess' in ele.text:
-            messy = True
+        messy = False
+        for ele in italics:
+            if 'Jon Mess' in ele.text:
+                messy = True
 
-    if messy:
-        for lyric in italics:
-            if lyric.text != 'Jon Mess':
-                messLyrics.append(lyric.text)
+        if messy:
+            for lyric in italics:
+                if lyric.text != 'Jon Mess':
+                    messLyrics.append(lyric.text)
 
-    openBracketElements = []
+        openBracketElements = []
 
-    for count, ele in enumerate(soupLyrics):
-        if '[' in ele.text:
-             openBracketElements.append(count)
+        for count, ele in enumerate(lyricSheet):
+            if '[' in ele.text:
+                 openBracketElements.append(count+1)
 
-    messElements = []
-    for count, ele in enumerate(soupLyrics):
-        if 'Jon Mess' in ele.text and '&' not in ele.text and '[' in ele.text:
-            messElements.append(count)
+        messElements = []
+        for count, ele in enumerate(lyricSheet):
+            if 'Jon Mess' in ele.text and '&' not in ele.text and '[' in ele.text:
+                messElements.append(count+1)
 
-    slices = []
-    for messNum in messElements:
-        for bCount, brackNum in enumerate(openBracketElements):
-            if brackNum == messNum and openBracketElements.index(brackNum) != len(openBracketElements)-1 and messNum != messElements[-1]:
-                slices.append([messNum, openBracketElements[bCount+1]])
-            elif messNum == messElements[-1] and brackNum == openBracketElements[-1]:
-                slices.append([messNum, "e"])
+        slice = []
+        for messNum in messElements:
+            for bCount, brackNum in enumerate(openBracketElements):
+                if brackNum == messNum and openBracketElements.index(brackNum) != len(openBracketElements)-1 and messNum != messElements[-1]:
+                    newSlice = [messNum, openBracketElements[bCount+1]]
+                    if newSlice:
+                        slice = newSlice
+                elif messNum == messElements[-1] and brackNum == openBracketElements[-1]:
+                    newSlice = [messNum, len(lyricSheet)-1]
+                    if newSlice:
+                        slice = newSlice
 
-    print(slices)
+        print(slice)
+        
+        if slice:
+            for count, ele in enumerate(lyricSheet):
+                for sl in range(slice[0], slice[1]):
+                    if count == sl and ele.text and ele.text not in notLyrics:
+                        print("Lyric number ", count, "is ", ele.text)
